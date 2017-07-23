d5cc22fbf00c5405d17885c305242e773a3000af
Tim Fleming <tfleming26@hotmail.com>
2017-07-22 21:37:01 -0600

    Create seeds file for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+  Blog.create!(
+    title: "Patrick's Blog Post #{blog}",
+    body: "Lorem Ipsum is simply dummy text of the printing and typesetting
+      industry. Lorem Ipsum has been the industry's standard dummy text ever since
+      the 1500s, when an unknown printer took a galley of type and scrambled it to
+      make a type specimen book. It has survived not only five centuries, but also
+      the leap into electronic typesetting, remaining essentially unchanged. It
+      was popularised in the 1960s with the release of Letraset sheets containing
+      Lorem Ipsum passages, and more recently with desktop publishing software
+      like Aldus PageMaker including versions of Lorem Ipsum."
+  )
+end
+
+puts "10 Blog Posts Created"
+
+9.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "My Awesome Business",
+    body: "At vero eos et accusamus et iusto odio dignissimos ducimus qui
+      blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et
+      quas molestias excepturi sint occaecati cupiditate non provident,
+      similique sunt in culpa qui officia deserunt mollitia animi, id est
+      laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita
+      distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque
+      nihil impedit quo minus id quod maxime placeat facere possimus, omnis
+      voluptas assumenda est, omnis dolor repellendus. Temporibus autem
+      quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet
+      ut et voluptates repudiandae sint et molestiae non recusandae. Itaque
+      earum rerum hic tenetur a sapiente delectus, ut aut reiciendis
+      voluptatibus maiores alias consequatur aut perferendis doloribus
+      asperiores repellat.",
+    main_image: "http://placehold.it/600x400",
+    thumb_image: "http://placehold.it/350x200"
+  )
+end
+
+puts "9 Portfolio Items Created"
