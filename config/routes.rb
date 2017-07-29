e6796f3fee607f7636bd458c8057668e79876b85
Tim Fleming <tfleming26@hotmail.com>
2017-07-29 09:10:31 -0600

    Add toggle status link for blog

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   
-  resources :blogs
+  resources :blogs do
+  	member do
+  	  get :post_status
+  	end
+  end
   
   root to: 'pages#home'
 end
